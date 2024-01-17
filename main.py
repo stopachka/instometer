c8c09f0718d53a1450fffef0ac98dc77606cd154
stopachka <stepan.p@gmail.com>
2024-01-17 18:48:46 -0500

    try trio

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import time
-import threading
 import os 
 import math 
-import websocket
 import json
-import backoff 
 import sys 
-import queue
+import trio 
+import trio_websocket 
 
 USE_REAL_HARDWARE = not os.environ.get('INSTOMETER_VIRTUAL_HARDWARE')
 
@@ -40,12 +38,6 @@ def set_shared_count(new_count):
 def get_shared_count():
     return shared_count 
 
-# ---------
-# Workers 
-
-shutdown_event = threading.Event()
-exception_queue = queue.Queue() 
-
 # -------------
 # Servo Worker 
 
@@ -76,109 +68,82 @@ def step_towards(start_angle, end_angle):
 
     return new_angle 
 
-def servo_worker():
-    try: 
-        set_servo_angle(0) 
-        print("[servo-worker] starting")
-        current_angle = 0
-        while not shutdown_event.is_set():
-            target_angle = count_to_angle(get_shared_count())
-            if (current_angle != target_angle): 
-                next_angle = step_towards(current_angle, target_angle) 
-                set_servo_angle(next_angle)
-                current_angle = next_angle
+async def servo_worker():
+    set_servo_angle(0) 
+    print("[servo-worker] starting")
+    current_angle = 0
+    while True:
+        target_angle = count_to_angle(get_shared_count())
+        if (current_angle != target_angle): 
+            next_angle = step_towards(current_angle, target_angle) 
+            set_servo_angle(next_angle)
+            current_angle = next_angle
 
-            time.sleep(0.01)
-    except Exception as e:
-        exception_queue.put(e)
+        await trio.sleep(0.01)
 
 # ---- 
 # OLED Worker 
 
-def oled_worker():
-    try:
-        draw_text("...") 
-        print("[oled-worker] starting")
-        last_count = None
-        while not shutdown_event.is_set():
-            current_count = get_shared_count() 
-            if current_count != last_count:
-                draw_text(f"{current_count}")
-                last_count = current_count 
-            time.sleep(0.01)
-    except Exception as e:
-        exception_queue.put(e)
+async def oled_worker():
+    draw_text("...") 
+    print("[oled-worker] starting")
+    last_count = None
+    while True:
+        current_count = get_shared_count() 
+        if current_count != last_count:
+            draw_text(f"{current_count}")
+            last_count = current_count 
+        await trio.sleep(0.01)
 
 # ------
 # Socket Worker 
 
 API_KEY = os.environ.get('INSTOMETER_API_KEY') 
 
-def on_message(ws, message):
-    print("[ws] message", message)
-    m = json.loads(message)
-    count = m['count']
-    set_shared_count(count)
+async def websocket_worker(): 
+    ws_uri = 'wss://api.instantdb.com/dash/session_counts'
+    async with trio_websocket.open_websocket_url(ws_uri) as ws:
+        print("[ws] connection opened")
+        init_message = json.dumps({
+            "type": "init", 
+            "token": API_KEY
+        })
+        await ws.send_message(init_message)
+        while True:
+            message = await ws.get_message()
+            print("[ws] message", message)
+            m = json.loads(message)
+            count = m['count']
+            set_shared_count(count)
 
-def on_error(ws, error):
-    print("[ws] error", error)
+async def reconnecting_websocket_worker(): 
+    while True: 
+        try:
+            await websocket_worker()
+        except trio_websocket.ConnectionClosed:
+            print("[ws] connection closed, reconnecting in 5s")
+            await trio.sleep(5) 
 
-def on_close(ws, close_status_code, close_msg):
-    print("[ws] connection closed")
-    raise websocket.WebSocketException("Connection closed")
-
-def on_open(ws):
-    print("[ws] connection opened")
-    init_message = json.dumps({
-        "type": "init", 
-        "token": API_KEY
-    })
-    ws.send(init_message)
-
-@backoff.on_exception(backoff.expo, websocket.WebSocketException, max_tries=12)
-def reconnecting_websocket(): 
-    ws = websocket.WebSocketApp("wss://api.instantdb.com/dash/session_counts",
-                                on_open=on_open,
-                                on_message=on_message,
-                                on_error=on_error,
-                                on_close=on_close)
-    ws.run_forever(ping_interval=10)
-
-def websocket_worker():
-    try:
-        reconnecting_websocket()
-    except Exception as e:
-        exception_queue.put(e)
 
 # ----
 # Main 
 
+async def main():
+    async with trio.open_nursery() as nursery:
+        nursery.start_soon(servo_worker)
+        nursery.start_soon(oled_worker)
+        nursery.start_soon(reconnecting_websocket_worker) 
    
 if __name__ == "__main__":
-    servo_thread = threading.Thread(target=servo_worker, daemon=True)
-    oled_thread = threading.Thread(target=oled_worker, daemon=True)
-    websocket_thread = threading.Thread(target=websocket_worker, daemon=True)
-    
     def shutdown_hardware():
-        shutdown_event.set() 
-        servo_thread.join()
-        oled_thread.join()
         set_servo_angle(0)
         draw_text("...")
-    
     try: 
-        servo_thread.start()
-        oled_thread.start()
-        websocket_thread.start()
-        
-        e = exception_queue.get() 
-        raise e 
-    
+        trio.run(main) 
     except KeyboardInterrupt:
         shutdown_hardware()
         print("goodbye :)")
         sys.exit(0)
-    
     except Exception as e:
         shutdown_hardware()
         draw_text("err :<")
