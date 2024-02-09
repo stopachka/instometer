a3eac0ace309f9d1477ee4959286bd5df4ac36c9
stopachka <stepan.p@gmail.com>
2024-02-09 08:15:28 -0500

    sm

--- servo.py
+++ servo.py
@@ -1,9 +1,9 @@
 from gpiozero import AngularServo
 from gpiozero.pins.pigpio import PiGPIOFactory
 
-min_pulse_width = 0.0005  #  0.5ms
-max_pulse_width = 0.0025  #  2.5ms
-factory = PiGPIOFactory() #  reduces jitter
+min_pulse_width = 0.00075  #  0.75ms
+max_pulse_width = 0.0025   #  2.5ms
+factory = PiGPIOFactory()  #  reduces jitter
 
 servo = AngularServo(
     17, 
