ecc97134971e55c6c9f8bdca39d6a4781880b33d
stopachka <stepan.p@gmail.com>
2024-03-05 14:43:07 -0500

    sm

--- log.py
+++ log.py
@@ -1,12 +1,23 @@
 import logging
+from logging.handlers import RotatingFileHandler
 
 FORMAT = "%(message)s"
+
+
+log_file_path = "log.txt"
+max_file_size_bytes = 10 * 1024 * 1024 # 10 MB
+backup_count = 2 
+rotating_handler = RotatingFileHandler(
+    log_file_path, 
+    maxBytes=max_file_size_bytes, 
+    backupCount=backup_count
+)
+
 logging.basicConfig(
-    filename="log.txt",
-    filemode="w",
     level="NOTSET", 
     format=FORMAT, 
     datefmt="[%X]", 
+    handlers=[rotating_handler]
 )
 
 log = logging.getLogger("main")
